5dca51b114a52e26df64f8fd8097ac3d0c36520b
Carlos Arango <47753930+carlosarango97@users.noreply.github.com>
2021-10-18 20:41:25 -0500

    Fibonnacci code

---
+++ Fibonnacci.py
@@ -0,0 +1,14 @@
+nterms = int(input("¿Cuantos terminos? "))
+n1 = 0
+n2 = 1
+count = 0
+
+if nterms == 1:
+   print(n1)
+else:
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count+=1
